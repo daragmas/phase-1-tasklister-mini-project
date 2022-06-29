23daebae4a563868f2ca880c2f2393c762043b98
daragmas <daragmas@gmail.com>
2022-06-29 11:33:19 -0400

    finished basic deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+  let newTaskForm = document.getElementById('create-task-form') //get task form
+  let newTaskDescription = document.getElementById('new-task-description') //gets task description
+  let taskList = document.getElementById('tasks') //get task list
+
+  newTaskForm.addEventListener("submit",(e)=> { //when submitted...
+    e.preventDefault() //stop refresh
+    //console.log(newTaskDescription.value)
+
+    //create new line item for task list, and add it to the list
+    //TODO: add remove button to the line item
+    let li = document.createElement('li') //make new task line item
+    li.textContent = newTaskDescription.value //set text of new line item to task descrition
+    
+    taskList.appendChild(li) // add new task to task list
+
+    newTaskForm.reset() // clear form
+  
+  })
+  });
